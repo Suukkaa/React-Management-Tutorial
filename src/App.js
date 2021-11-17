ed70cef7a4bb90f6c1358a0774f033c9edbc62bf
Su <suokkaa@gmail.com>
2021-11-17 14:37:44 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Customer from "./components/Customer";
+
+const customers = [
+  {
+    id: 1,
+    image: "https://placeimg.com/128/128/1",
+    name: "김현수",
+    birthday: "952501",
+    gender: "남자",
+    job: "비둘기",
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/128/128/2",
+    name: "현수",
+    birthday: "951301",
+    gender: "남자",
+    job: "비둘기",
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/128/128/3",
+    name: "수",
+    birthday: "951001",
+    gender: "남자",
+    job: "비둘기",
+  },
+];
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Develop management system! </h2>      
+    <div>
+      {customers.map((c) => {
+        return (
+          <Customer
+            key={c.id}
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birthday={c.birthday}
+            gender={c.gender}
+            job={c.job}
+          />
+        );
+      })}
     </div>
   );
 }
